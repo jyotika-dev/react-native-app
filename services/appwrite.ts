7c9611b330226370887088b6f1d64698b6037d80
Jyotika Kishor <jkishor1802@gmail.com>
2025-03-29 13:32:36 +0530

    updated commits

---
+++ services/appwrite.ts
@@ -0,0 +1,61 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+//track the searches made by the user
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  // check if a record of that search has already been stored
+  // if a document is found then increment the search count
+  // if no document is found then a new document is created in appwrite database -> 1
+
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovies = result.documents[0];
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovies.$id,
+        {
+          count: existingMovies.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        count: 1,
+        movie_id: movie.id,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
